2c19ee74ee58087741fa0f4a026ff38f2f73cc39
akhileshravikumar <akhi2431@gmail.com>
2026-09-11 10:50:51 +0100

    twosum test

---
+++ sandbox/twosum.py
@@ -0,0 +1,9 @@
+def twoSum(nums: list[int], target: int) -> list[int]:
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[j] == target - nums[i]:
+                return [i, j]
+    return []
+
+
+print(twoSum([2, 7, 11, 15], 9))  # Output: [0, 1] 
